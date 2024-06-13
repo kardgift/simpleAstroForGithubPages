2f298b3196371fa332ae1eeee0d184d9697efdac
whoami <who@am.i>
2024-06-13 12:54:44 +0400

    kir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yumyooyum.github.io/',
+    // site: 'https://yumyooyum.github.io/',
+    // base: '/simpleAstroForGithubPages/',
+    site: 'https://kardgift.github.io/',
     base: '/simpleAstroForGithubPages/',
 });
